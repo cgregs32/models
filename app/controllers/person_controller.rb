9e586d3c3653ecdd96aca40bd4703f6eda860875
cgregs32 <cgregs32@gmail.com>
2017-11-28 14:24:04 -0700

    add people model, person controller, index new edit show views

---
+++ app/controllers/person_controller.rb
@@ -0,0 +1,13 @@
+class PersonController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'person/index'
+
+  get 'person/new'
+
+  get 'person/show'
+
+  get 'person/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
